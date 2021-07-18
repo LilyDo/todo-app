eab5915c8c7a08570751aa848928ceccd567584b
Lily Do <dothuthao1504@gmail.com>
2021-07-19 03:04:26 +0800

    Implement basic functionality todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [todoInput, setTodoInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addItem = () => {
+    const todoObject = { id: uuidv4(), value: todoInput };
+    setTodoList([...todoList, todoObject]);
+    setTodoInput("");
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addItem();
+    }
+  };
+
+  const markAsDone = (itemToMarkAsDone) => {
+    const index = todoList.findIndex((item) => item.id === itemToMarkAsDone.id);
+    todoList[index].done = true;
+    setTodoList([...todoList]);
+  };
+
+  const deleteTodo = (itemToDelete) => {
+    const index = todoList.findIndex((item) => item.id === itemToDelete.id);
+    todoList.splice(index, 1);
+    setTodoList([...todoList]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TO DO app</h1>
+      <input
+        type="text"
+        placeholder="add to do item"
+        value={todoInput}
+        onChange={(e) => setTodoInput(e.target.value)}
+        onKeyPress={onKeyPress}
+      ></input>
+      <button onClick={addItem}>Add</button>
+      <ul>
+        {todoList.map((item) => (
+          <li key={item.id}>
+            <input type="checkbox" onClick={() => markAsDone(item)} />
+            <span className={item.done ? "done" : ""}>{item.value}</span>
+            <input
+              type="button"
+              onClick={() => deleteTodo(item)}
+              value="delete"
+            />
+          </li>
+        ))}
+      </ul>{" "}
     </div>
   );
 }
